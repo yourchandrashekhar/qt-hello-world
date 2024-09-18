34bbc9c31306417145194b8eadd61652904685dd
Chandra Shekhar <yourchandrashekhar@gmail.com>
2024-09-19 00:03:32 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,54 @@
 #include <QApplication>
-#include <QPushButton>
+#include <QtWidgets/QWidget>
+#include <Qt3DCore/QEntity>
+#include <Qt3DExtras/Qt3DWindow>
+#include <Qt3DExtras/QOrbitCameraController>
+#include <Qt3DExtras/QSphereMesh>
+#include <Qt3DExtras/QPhongMaterial>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv)
+{
     QApplication app(argc, argv);
-    QPushButton button("Hello, World!");
-    button.show();
+
+    // Create a Qt3D window
+    Qt3DExtras::Qt3DWindow *view = new Qt3DExtras::Qt3DWindow();
+    view->defaultFrameGraph()->setClearColor(QColor(QRgb(0x4d4d4f)));
+    QWidget *container = QWidget::createWindowContainer(view);
+    container->setMinimumSize(QSize(200, 100));
+    container->setMaximumSize(QSize(800, 600));
+    container->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    container->setFocusPolicy(Qt::StrongFocus);
+
+    // Root entity
+    Qt3DCore::QEntity *rootEntity = new Qt3DCore::QEntity();
+
+    // Camera
+    Qt3DRender::QCamera *cameraEntity = view->camera();
+
+    // For camera controls
+    Qt3DExtras::QOrbitCameraController *camController = new Qt3DExtras::QOrbitCameraController(rootEntity);
+    camController->setLinearSpeed(50.0f);
+    camController->setLookSpeed(180.0f);
+    camController->setCamera(cameraEntity);
+
+    // Sphere
+    Qt3DCore::QEntity *sphereEntity = new Qt3DCore::QEntity(rootEntity);
+    Qt3DExtras::QSphereMesh *sphereMesh = new Qt3DExtras::QSphereMesh();
+    sphereMesh->setRings(20);
+    sphereMesh->setSlices(20);
+    sphereMesh->setRadius(2);
+
+    Qt3DExtras::QPhongMaterial *material = new Qt3DExtras::QPhongMaterial();
+    material->setDiffuse(QColor(QRgb(0xbeb32b)));
+
+    sphereEntity->addComponent(sphereMesh);
+    sphereEntity->addComponent(material);
+
+    // Set root object of the scene
+    view->setRootEntity(rootEntity);
+
+    // Show window
+    container->show();
+
     return app.exec();
 }
